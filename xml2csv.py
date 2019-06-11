25227df9346e454af35ba9af60c8d3534aac9dbb
jhvics1 <jhvics1@gmail.com>
2019-06-11 17:40:20 +0900

    Update xml2csv.py

--- xml2csv.py
+++ xml2csv.py
@@ -1,4 +1,5 @@
 import os
+import glob
 import argparse
 import xml.etree.ElementTree
 
@@ -22,7 +23,7 @@ import xml.etree.ElementTree
 # < / object >
 
 
-def transform_xml_to_csv(image_path, label_path):
+def transform_xml_to_csv1(image_path, label_path):
     fout = open(label_path + '/../annotation.csv', 'wt')
     for path in sorted(os.listdir(label_path)):
         file_path = os.path.join(label_path, path)
@@ -56,6 +57,46 @@ def transform_xml_to_csv(image_path, label_path):
 
     fout.close()
 
+
+def transform_xml_to_csv(image_path, label_path):
+    fout = open(label_path + '/../annotation.csv', 'wt')
+    for file_name in sorted(os.listdir(label_path)):
+        file_path = os.path.join(label_path, file_name)
+
+        try:
+            doc = xml.etree.ElementTree.parse(file_path)
+            root = doc.getroot()
+            # path = root.find('path').text.split('\\')
+            file_name, _ = os.path.splitext(file_name)
+            image_path = os.path.abspath(image_path)
+            # print('name ====> '+file_name)
+            # print('imgpath ====> ' + image_path)
+            for item in glob.glob(image_path + '/*'):
+                # print('item ====> ' + item)
+                if file_name in item:
+                    # path = os.path.abspath(file)
+                    path = os.path.abspath(item)
+                    # print('path : {}'.format(path.text))
+                    for obj in root.findall('object'):
+                        name = obj.find('name')
+                        # print('name : {}'.format(name.text))
+                        box = obj.find('bndbox')
+                        if int(box.find('xmin').text) < int(box.find('xmax').text) and int(box.find('ymin').text) < int(
+                                box.find('ymax').text):
+                            transform = '{fname},{x1},{y1},{x2},{y2},{class_name}\n'.format(fname=path,
+                                                                                            x1=box.find('xmin').text,
+                                                                                            y1=box.find('ymin').text,
+                                                                                            x2=box.find('xmax').text,
+                                                                                            y2=box.find('ymax').text,
+                                                                                            class_name=name.text)
+                            fout.write(str(transform))
+                            fout.flush()
+                            # print(transform)
+                break
+        except:
+            print('Mal-formed file : {} - {}'.format(file_path, root))
+
+    fout.close()
 
 def print_args():
     parser = argparse.ArgumentParser('Runs 2 Stream inflated 3D ConvNet '
